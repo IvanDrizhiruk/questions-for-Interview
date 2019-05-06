9ff8849129fd413bc9e98639e8c71eb174a49a15
Ivan Dryzhyruk <IDryzhyruk@luxoft.com>
2019-05-06 15:20:28 +0300

    Init commit for maven app

---
+++ src/main/java/ua/dp/question/interview/App.java
@@ -0,0 +1,7 @@
+package ua.dp.question.interview;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/ua/dp/question/interview/AppTest.java
@@ -0,0 +1,13 @@
+package ua.dp.question.interview;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
